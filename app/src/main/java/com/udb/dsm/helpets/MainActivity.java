746b25050bfd80d0a53af70200194c3e2132e044
Jaime Navarrete <jaimenava123@outlook.com>
2021-03-24 12:03:25 -0600

    Finalizada - Actividad de Splash Screen

--- app/src/main/java/com/udb/dsm/helpets/MainActivity.java
+++ app/src/main/java/com/udb/dsm/helpets/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udb.dsm.helpets;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,23 +14,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
